987ac20c3ded7c2e3902a112a2b0bd05ff285f94
新明瑞騎 <106291585+MizukiShinmyou@users.noreply.github.com>
2023-06-05 15:53:40 +0900

    20230605

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,46 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update]
   def index
    @articles = Article.all
   end
+
+  def show  
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit  
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_url(@article), notice: 'Article was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
+
